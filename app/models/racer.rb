798e47a13982608a7bb620e1ea75e585d5337bc1
Cathy McDonald <cthmcdnld@gmail.com>
2017-04-03 16:49:37 +1000

    Used Rubocop to correct style

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,22 +1,22 @@
+# Racer model class which integrates with MongoDB
 class Racer
   include Mongoid::Document
-  
+
   attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
-   
+
   def self.mongo_client
     Mongoid::Clients.default
   end
-  
+
   def self.collection
-    self.mongo_client[:racers]
+    mongo_client[:racers]
   end
-  
-  def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil)
-    if (limit)
-      self.collection.find(prototype).skip(skip).sort(sort).limit(limit)
+
+  def self.all(prototype = {}, sort = { number: 1 }, skip = 0, limit = nil)
+    if limit
+      collection.find(prototype).skip(skip).sort(sort).limit(limit)
     else
-      self.collection.find(prototype).skip(skip).sort(sort)
-   end
+      collection.find(prototype).skip(skip).sort(sort)
+    end
   end
 end
-
