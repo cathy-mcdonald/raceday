f9505ea0534febe7427b95081594789670481e70
Cathy McDonald <cthmcdnld@gmail.com>
2017-04-04 19:34:39 +1000

    Completed CRUD exercises

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -19,4 +19,46 @@ class Racer
       collection.find(prototype).skip(skip).sort(sort)
     end
   end
+
+  def initialize(params = {})
+    @id = params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number = params[:number].to_i
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+  end
+
+  def self.find(id)
+    result = collection.find(_id: id).first ||
+             collection.find(_id: BSON::ObjectId.from_string(id)).first
+    result.nil? ? nil : Racer.new(result)
+  end
+
+  def save
+    result = collection.insert_one(number: @number,
+                                   first_name: @first_name,
+                                   last_name: @last_name,
+                                   gender: @gender,
+                                   group: @group,
+                                   secs: @secs)
+    @id = result.inserted_id.to_s
+  end
+
+  def update(params)
+    @number = params[:number].to_i
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)
+    collection.find(_id: BSON::ObjectId.from_string(@id)).update_one(params)
+  end
+
+  def destroy
+    collection.find(_id: BSON::ObjectId.from_string(@id)).delete_one
+  end
 end
