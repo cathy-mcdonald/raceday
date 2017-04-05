6e2270d47f24f771991da4883f773bca47632480
Cathy McDonald <cthmcdnld@gmail.com>
2017-04-05 10:06:05 +1000

    Completed pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :racers
+  
   root 'racers#index'
   
-  resources :racers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
